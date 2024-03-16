e0fbc5401bed59e675073af7734ac45122277f0b
miguel angel <miguelangelrodriguezpulgarin2@gmail.com>
2024-03-15 22:07:01 -0500

    Agregar archivos de distribución

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: 'https://Angelpulgarin369.github.io/proyecto-UNI'
+
+});
